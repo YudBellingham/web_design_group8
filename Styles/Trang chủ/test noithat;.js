f8bb79e4cdaf0eacdbc2758fbc844f37432c89a8
YudBellingham <quanduysoma1@gmail.com>
2024-12-25 19:34:37 +0700

    web design

--- Styles/Trang chủ/test noithat;.js
+++ Styles/Trang chủ/test noithat;.js
@@ -3,8 +3,24 @@ let cart = [];
 function addToCart(product) {
     cart.push(product);
     alert(`${product} đã được thêm vào giỏ hàng!`);
-    console.log(cart); // Debug để kiểm tra danh sách giỏ hàng
+    console.log(cart); 
 }
+
+function showNotification() {
+    alert("Tính năng này sẽ được cập nhật sau!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
